8db08ccb8a3a76b80ae1c8b32f5a008a162f69eb
Thang PHAM <phamthang37@yahoo.com>
2022-03-31 14:32:46 +0200

    3e commit

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.listview;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.listview.controller.PersonDao;
 import com.example.listview.model.Person;
@@ -19,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText surnameEditText;
     private EditText nameEditText;
 
+    // declare a launcher to call an intent to start
+    // an execution process ActivityResultContracts
+    ActivityResultLauncher<Intent> intentLauncher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +39,26 @@ public class MainActivity extends AppCompatActivity {
         // reference to UI components
         surnameEditText = findViewById(R.id.surnameEditText);
         nameEditText = findViewById(R.id.nameEditText);
+
+        // GetContent creates an ActivityResultLauncher<String> to allow you to pass
+        // in the mime type you'd like to allow the user to select
+        intentLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    // Check whether result is OK
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // retrieve the intent return by second Activity
+                        Intent data = result.getData();
+
+                        // get person in intent
+                        Person person = (Person) data.getSerializableExtra(SecondActivity.PERSON_KEY);
+                        Log.d("App", "Return person" + person.toString());
+                        Toast.makeText(getBaseContext(), person.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
 
     }
 
@@ -48,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(PERSON_LIST_KEY, (Serializable) PersonDao.getAllPersons());
 
         // start second activity with message
-        startActivity(intent);
+        // startActivity(intent);
+        intentLauncher.launch(intent); // launch with a call back to process return data
     }
 }
--- app/src/main/java/com/example/listview/SecondActivity.java
+++ app/src/main/java/com/example/listview/SecondActivity.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import com.example.listview.controller.PersonDao;
 import com.example.listview.model.Person;
 
 import java.io.Serializable;
@@ -19,6 +21,7 @@ import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
+    public static final String PERSON_KEY = "Person";
     // UI
     private ListView personListView;
     // define adapter pour list view
@@ -39,5 +42,24 @@ public class SecondActivity extends AppCompatActivity {
         // create Adapter
         personListViewAdapter = new ArrayAdapter<Person>(this, android.R.layout.simple_list_item_1, persons);
         personListView.setAdapter(personListViewAdapter);
+
+        personListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // retrieve person clicked
+                Person person = PersonDao.getAllPersons().get(position);
+
+                // create an intent to sent message to main activity
+                Intent intent = new Intent();
+                intent.putExtra(PERSON_KEY, person);
+
+                // set a code OK to send to the main activity
+                setResult(RESULT_OK, intent);
+
+                // close Second Activity, back to previous activity, i.e. MainActivity
+                finish();
+
+            }
+        });
     }
 }
