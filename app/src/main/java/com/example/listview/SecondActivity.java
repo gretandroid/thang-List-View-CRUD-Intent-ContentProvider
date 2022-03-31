9a96f897d991852eeb33096d0d1d914ba62b4399
Thang PHAM <phamthang37@yahoo.com>
2022-03-31 11:51:08 +0200

    2e commit

--- app/src/main/java/com/example/listview/SecondActivity.java
+++ app/src/main/java/com/example/listview/SecondActivity.java
@@ -2,13 +2,42 @@ package com.example.listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.listview.model.Person;
+
+import java.io.Serializable;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
+
+    // UI
+    private ListView personListView;
+    // define adapter pour list view
+    private ListAdapter personListViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // reference to UI element
+        personListView = findViewById(R.id.personListView);
+
+        // get intent message sent by main activity
+        Intent intent = getIntent();
+        List<Person> persons = (List<Person>) intent.getSerializableExtra(MainActivity.PERSON_LIST_KEY);
+
+        // create Adapter
+        personListViewAdapter = new ArrayAdapter<Person>(this, android.R.layout.simple_list_item_1, persons);
+        personListView.setAdapter(personListViewAdapter);
     }
 }
